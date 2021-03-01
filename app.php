96741fc1e4eada7d59acf457175b1c9be7367f81
code99-ash <oyetikh1@gmail.com>
2021-03-01 12:01:59 +0100

    there is just an interface with necessary packages installed

---
+++ app.php
@@ -0,0 +1,11 @@
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\Socket;
+
+require 'vendor/autoload.php';
+
+$server = IoServer::factory(new HttpServer(new WsServer(new Socket())), 8082);
+
+$server->run();
